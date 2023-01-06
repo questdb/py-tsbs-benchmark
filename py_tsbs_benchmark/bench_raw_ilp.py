9cf689617dddc4ae6edc7190a9781171123a424a
Adam Cimarosti <cimarosti@gmail.com>
2023-01-06 11:10:35 +0000

    feat: Ahead of time table creation, table cleanup and waiting until all results are queriable.

--- py_tsbs_benchmark/bench_raw_ilp.py
+++ py_tsbs_benchmark/bench_raw_ilp.py
@@ -2,6 +2,8 @@ import socket
 import time
 import struct
 
+from .common import CpuTable
+
 
 class RawFileSender:
     def __init__(self, host, port, file_path, chunk_size=64 * 1024):
@@ -13,6 +15,8 @@ class RawFileSender:
     def send(self):
         with open(self.file_path, 'rb') as f:
             buf = f.read()
+
+        row_count = buf.count(b'\n')
         
         view = memoryview(buf)
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -27,21 +31,41 @@ class RawFileSender:
                 sock.sendall(view[i:i + self.chunk_size])
         t1 = time.monotonic()
         elapsed = t1 - t0
+        row_speed = row_count / elapsed / 1_000_000.0
         size_mb = len(buf) / 1024.0 / 1024.0
         throughput_mb = size_mb / elapsed
         print('Sent:')
         print(
+            f'  {row_count} rows in {elapsed:.2f}s: '
+            f'{row_speed:.2f} mil rows/sec.')
+        print(
             f'  {len(buf)} bytes in {elapsed:.2f}s: '
             f'{throughput_mb:.2f} MiB/sec.')
 
+        return row_count
 
-def main():
+
+def parse_args():
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--host', type=str, default='localhost')
-    parser.add_argument('--port', type=int, default=9009)
+    parser.add_argument('--ilp-port', type=int, default=9009)
+    parser.add_argument('--http-port', type=int, default=9000)
     parser.add_argument('--chunk-size', type=int, default=64 * 1024)
     parser.add_argument('file_path', type=str)
-    args = parser.parse_args()
-    sender = RawFileSender(args.host, args.port, args.file_path)
-    sender.send()
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    print(f'Running with params: {vars(args)}')
+
+    cpu_table = CpuTable(args.host, args.http_port)
+    cpu_table.drop()
+    cpu_table.create()
+    time.sleep(1)  # grace period.
+
+    sender = RawFileSender(args.host, args.ilp_port, args.file_path)
+    row_count = sender.send()
+
+    cpu_table.block_until_rowcount(row_count)
---
+++ py_tsbs_benchmark/common.py
@@ -0,0 +1,63 @@
+import requests
+import time
+
+class CpuTable:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+
+    def _request(self, sql):
+        response = requests.get(
+            f'http://{self.host}:{self.port}/exec',
+            params={'query': sql}).json()
+        return response
+
+    def drop(self):
+        response = self._request('drop table cpu')
+        if response.get('ddl') == 'OK':
+            print(f'Dropped table cpu')
+            return True
+        elif response.get('error', '').startswith('table does not exist'):
+            print(f'Table cpu does not exist')
+            return False
+        else:
+            raise RuntimeError(f'Failed to drop table cpu: {response}')
+
+    def create(self):
+        symbol_cols = [
+            'hostname', 'region', 'datacenter', 'rack', 'os', 'arch',
+            'team', 'service', 'service_version', 'service_environment']
+        double_cols = [
+            'usage_user', 'usage_system', 'usage_idle', 'usage_nice',
+            'usage_iowait', 'usage_irq', 'usage_softirq', 'usage_steal',
+            'usage_guest', 'usage_guest_nice']
+        sql = f'''
+            create table cpu (
+                {', '.join(f'{col} symbol' for col in symbol_cols)},
+                {', '.join(f'{col} double' for col in double_cols)},
+                timestamp timestamp)
+                    timestamp(timestamp)
+            '''
+        response = self._request(sql)
+        if response.get('ddl') == 'OK':
+            print(f'Created table cpu')
+        else:
+            raise RuntimeError(f'Failed to create table cpu: {response}')
+
+    def get_row_count(self):
+        response = self._request('select count(*) from cpu')
+        return response['dataset'][0][0]
+
+    def block_until_rowcount(self, target_count, timeout=30.0):
+        t0 = time.monotonic()
+        while True:
+            row_count = self.get_row_count()
+            if row_count == target_count:
+                return
+            elif row_count > target_count:
+                raise RuntimeError(
+                    f'Row count {row_count} exceeds target {target_count}')
+            if time.monotonic() - t0 > timeout:
+                raise RuntimeError(
+                    f'Timed out waiting for row count to reach {target_count}')
+            time.sleep(0.1)
